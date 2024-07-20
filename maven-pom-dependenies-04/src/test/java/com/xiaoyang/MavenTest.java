96e16488a88da80ed5c3884529201199c7c919ae
XiaoYang <xiaoyang020305@outlook.com>
2024-07-20 10:51:54 +0800

    MavenTest

---
+++ maven-pom-dependenies-04/src/test/java/com/xiaoyang/MavenTest.java
@@ -0,0 +1,20 @@
+package com.xiaoyang;
+
+import org.junit.Test;
+
+/**
+ * ClassName:MavenTest
+ * Package:com.xiaoyang
+ * Description:
+ *
+ * @Author:XiaoYAng :2024/7/19 - 8:18
+ * @Version:v1.8
+ */
+public class MavenTest {
+
+
+    @Test
+    public void test(){
+
+    }
+}
